21f0fd42facf79c58627a7357e7a8c683c7aa75b
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-04-29 16:00:31 +0200

    Added login && register

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class UserApiController extends Controller
 {
@@ -116,5 +117,86 @@ class UserApiController extends Controller
         $user->delete();
 
         return response()->json(['message' => 'Resource deleted successfully', 'status' => 'success'], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => [
+                'required',
+                'string',
+            ]
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            if(auth('sanctum')->check()){
+                auth()->user()->tokens()->delete();
+            }
+            $token = Auth::user()->createToken('app_token_'.Auth::user()->id,[
+                'create',
+                'read',
+                'update',
+                'delete',
+            ])->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login successful',
+                'status' => 'success',
+                'data' => [
+                    "user" => Auth::user(),
+                    "token" => $token
+                ]
+            ]);
+        } else {
+            return response()->json(['message' => 'Unauthorized', 'status' => 'error'], 401);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => [
+                'required',
+                'string',
+                'min:8',// Minimum length of 8 characters
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]+$/', // At least one uppercase letter, one lowercase letter, one number, and one special character
+                'confirmed', // Requires matching password_confirmation field
+            ],
+            'password_confirmation' => [
+                'required',
+                'string',
+                'min:8',// Minimum length of 8 characters
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]+$/', // At least one uppercase letter, one lowercase letter, one number, and one special character
+            ],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create($request->all());
+
+        $token = $user->createToken('app_token_'.$user->id,[
+            'create',
+            'read',
+            'update',
+            'delete',
+        ])->plainTextToken;
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'status' => 'success',
+            'data' => [
+                "user" => $user,
+                "token" => $token
+            ]
+        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,26 @@ Route::prefix('v1')->group(function () {
         ]);
     });
 
-    Route::resource('products', ProductApiController::class);
+    Route::resource('products', ProductApiController::class)
+        ->name('index', 'products.index')
+        ->name('show', 'products.show')
+        ->name('store', 'products.store')
+        ->name('update', 'products.update')
+        ->name('destroy', 'products.delete');
     // Route::get('/products', [ProductApiController::class, 'index'])->name('products.index');
     // Route::get('/products/{id}', [ProductApiController::class, 'show'])->name('products.show');
     // Route::post('/products', [ProductApiController::class, 'store'])->name('products.store');
     // Route::put('/products/{id}', [ProductApiController::class, 'update'])->name('products.update');
     // Route::delete('/products/{id}', [ProductApiController::class, 'destroy'])->name('products.destroy');
 
-    Route::resource('users', UserApiController::class);
+    Route::resource('users', UserApiController::class)
+        ->name('index', 'users.index')
+        ->name('show', 'users.show')
+        ->name('store', 'users.store')
+        ->name('update', 'users.update')
+        ->name('destroy', 'users.delete');
+
+    Route::post('/login', [UserApiController::class, 'login'])->name('users.login');
+    Route::post('/register', [UserApiController::class, 'register'])->name('users.register');
 });
 
