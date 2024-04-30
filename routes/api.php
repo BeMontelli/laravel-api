a7d8e8ccfae1b49ac304d731eb9ec5748cb9ab07
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-04-30 09:32:59 +0200

    removed comments

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,6 @@ Route::prefix('v1')->group(function () {
         ->name('store', 'products.store')
         ->name('update', 'products.update')
         ->name('destroy', 'products.delete');
-    // Route::get('/products', [ProductApiController::class, 'index'])->name('products.index');
-    // Route::get('/products/{id}', [ProductApiController::class, 'show'])->name('products.show');
-    // Route::post('/products', [ProductApiController::class, 'store'])->name('products.store');
-    // Route::put('/products/{id}', [ProductApiController::class, 'update'])->name('products.update');
-    // Route::delete('/products/{id}', [ProductApiController::class, 'destroy'])->name('products.destroy');
 
     Route::resource('users', UserApiController::class)
         ->middleware('auth:sanctum')
