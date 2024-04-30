9f42a48930204e33bc5fc346fc229f78c2dd9b23
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-04-30 09:24:10 +0200

    Removed /user api endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductApiController;
 use App\Http\Controllers\UserApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::prefix('v1')->group(function () {
 
     Route::get('/welcome', function (Request $request) {
